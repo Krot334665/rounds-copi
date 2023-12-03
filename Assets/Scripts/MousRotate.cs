aa852c0e107c4334dcd24facf8c86a34bad548a8
Krot_228 <mark.kiktenko@gmail.com>
2023-12-03 12:21:46 +0100

    BaseProject

---
+++ Assets/Scripts/MousRotate.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class MousRotate : MonoBehaviour
+{
+    // Установите желаемую высоту объекта относительно экрана
+    public float height = 1.0f;
+    void Update()
+    {
+        // Получаем позицию мыши в экранных координатах
+        Vector3 mousePosition = Input.mousePosition;
+
+        // Добавляем высоту относительно экрана
+        mousePosition.z = height;
+
+        // Преобразуем позицию мыши из экранных в мировые координаты
+        Vector3 targetPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        // Находим направление курсора от объекта
+        Vector3 direction = targetPosition - transform.position;
+
+        // Находим угол поворота в радианах
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        // Если угол поворота больше 180 градусов, отзеркаливаем объект
+        if (Mathf.Abs(angle) > 90)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            angle -= 180; // Корректируем угол после отзеркаливания
+        }
+        else
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        // Создаем кватернион поворота по оси Z
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        // Применяем поворот к объекту
+        transform.rotation = rotation;
+
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Map"))
+        {
+            Destroy(collision.gameObject);
+        }
+        Destroy(gameObject);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
