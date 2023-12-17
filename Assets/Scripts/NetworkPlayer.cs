eb84d89eb40da5e4bf2037d81aad35caf29ca44c
Krot_228 <mark.kiktenko@gmail.com>
2023-12-17 20:14:32 +0100

    change player prefab and delete olg controller

--- Assets/Scripts/NetworkPlayer.cs
+++ Assets/Scripts/NetworkPlayer.cs
@@ -1,4 +1,4 @@
-using Unity.Netcode;
+﻿using Unity.Netcode;
 using UnityEngine;
 
 namespace HelloWorld
@@ -6,6 +6,18 @@ namespace HelloWorld
     public class NetworkPlayer : NetworkBehaviour
     {
         public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>();
+        public float speed = 5f;
+        public float jumpForce = 10f;
+        public Transform groundCheck;
+        public LayerMask groundLayer;
+
+        private Rigidbody2D rb;
+        private bool isGrounded;
+
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
 
         public override void OnNetworkSpawn()
         {
@@ -14,6 +26,7 @@ namespace HelloWorld
                 Spawn();
             }
         }
+
         public void Spawn()
         {
             if (IsOwner)
@@ -23,12 +36,39 @@ namespace HelloWorld
             {
                 Position.Value = new Vector3(-6, -1.7f, 0);
             }
+            transform.position = Position.Value;
         }
 
 
         void Update()
         {
-            transform.position = Position.Value;
+            Position.Value = transform.position;
+            // Проверка, находится ли персонаж на земле
+                    isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+
+                    // Обработка ввода для движения и прыжка
+                    float horizontalInput = Input.GetAxis("Horizontal");
+                    HandleMovement(horizontalInput);
+
+                    if (isGrounded && Input.GetButtonDown("Jump"))
+                    {
+                        Jump();
+                    }
+                
+        }
+
+
+        private void HandleMovement(float horizontalInput)
+        {
+            // Движение влево/вправо
+            Vector2 movement = new Vector2(horizontalInput * speed, rb.velocity.y);
+            rb.velocity = movement;
+        }
+
+        private void Jump()
+        {
+            // Прыжок
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
     }
 }
