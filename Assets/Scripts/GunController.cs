207aa6236d59fd84b1b053c9be9351bd72bd6948
Tim <devsspacegame@gmail.com>
2024-01-21 12:26:14 +0200

    spawn and despawn projectail

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -1,29 +1,73 @@
+using Unity.Netcode;
 using UnityEngine;
 
-public class GunController : MonoBehaviour
+public class GunController : NetworkBehaviour
 {
     [SerializeField] GameObject projectilePrefab;
     [SerializeField] Transform spawnPos;
     [SerializeField] Transform playerPos;
     [SerializeField] float spawnForce;
+    private GameObject projectile;
 
     void Update()
     {
+        if (!IsOwner)
+        {
+            return;
+        }
+        
         if (Input.GetMouseButtonDown(0))
         {
-            // GameObject projectile = Instantiate(projectilePrefab, spawnPos.position, Quaternion.identity);
-            // Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
-            // if (rb != null)
-            // {
-            //     Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            //     Vector2 weaponPosition = playerPos.position;
-            //
-            //     Vector2 direction = (mousePosition - weaponPosition).normalized;
-            //     rb.velocity = direction * spawnForce;
-            // }else
-            // {
-            //     Debug.Log("erorrr");
-            // }
+            InstantiateProjectileServerRpc(spawnPos.position);
+
+            SetVelocityServerRpc();
+        }
+    }
+
+    private void SpawnProjectile(Vector3 position)
+    {
+        projectile = Instantiate(projectilePrefab, position, Quaternion.identity);
+        projectile.GetComponent<NetworkObject>().Spawn();
+    }
+
+    private void Shoot()
+    {
+        Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
+
+        if (rb != null)
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 weaponPosition = playerPos.position;
+
+            Vector2 direction = (mousePosition - weaponPosition).normalized;
+            rb.velocity = direction * spawnForce;
+        }
+        else
+        {
+            Debug.Log("erorrr");
+        }
+    }
+
+    [ServerRpc]
+    public void SetVelocityServerRpc(ServerRpcParams serverRpcParams = default)
+    {
+        var clientId = serverRpcParams.Receive.SenderClientId;
+        if (NetworkManager.ConnectedClients.ContainsKey(clientId))
+        {
+            var client = NetworkManager.ConnectedClients[clientId];
+            Shoot();
+        }
+    }
+    
+    [ServerRpc]
+    public void InstantiateProjectileServerRpc(Vector3 position, ServerRpcParams serverRpcParams = default)
+    {
+        var clientId = serverRpcParams.Receive.SenderClientId;
+        if (NetworkManager.ConnectedClients.ContainsKey(clientId))
+        {
+            var client = NetworkManager.ConnectedClients[clientId];
+            Debug.Log($"pos {spawnPos.position}");
+            SpawnProjectile(position);
         }
     }
 }
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,14 +1,37 @@
+using Unity.Netcode;
 using UnityEngine;
 
-public class Projectile : MonoBehaviour
+public class Projectile : NetworkBehaviour
 {
-    // Start is called before the first frame update
+    private Collision2D collision2D;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (!collision.gameObject.CompareTag("Map"))
-        {
-            Destroy(collision.gameObject);
-        }
-        Destroy(gameObject);
+        collision2D = collision;
+        DespawnProjectileServerRpc();
     }
+
+     private void DespawnProjectile(Collision2D collision)
+     {
+         if (!collision.gameObject.CompareTag("Map"))
+         {
+             collision.gameObject.GetComponent<NetworkObject>().DontDestroyWithOwner = true;
+             collision.gameObject.GetComponent<NetworkObject>().Despawn();
+         }
+
+         gameObject.GetComponent<NetworkObject>().DontDestroyWithOwner = true;
+         gameObject.GetComponent<NetworkObject>().Despawn();
+     }
+
+
+     [ServerRpc]
+     public void DespawnProjectileServerRpc(ServerRpcParams serverRpcParams = default)
+     {
+         var clientId = serverRpcParams.Receive.SenderClientId;
+         if (NetworkManager.ConnectedClients.ContainsKey(clientId))
+         {
+             var client = NetworkManager.ConnectedClients[clientId];
+             DespawnProjectile(collision2D);
+         }
+     }
 }
